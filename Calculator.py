4f32cf1258b4ae40a1a7d56a6c577027c937fb2b
Shaalaa Gyan <115674850+suyashsingh09@users.noreply.github.com>
2023-06-08 09:54:17 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+operator=input("Enter the operator (+,-,*,/,%,//): ")
+
+if operator=="+":
+    print(num1+num2)
+if operator=="-":
+    print(num1-num2) 
+if operator=="*":
+    print(num1*num2)
+if operator=="/":
+    print(num1/num2)
+if operator=="%":
+    print(num1%num2)
+if operator=="//":
+    print(num1//num2)                   
